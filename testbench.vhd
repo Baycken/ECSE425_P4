35b7e9b55927142db4d4a4b1639a0b24cc8e6be4
IsaacBerman <isaac.berman96@gmail.com>
2018-03-26 12:31:05 -0400

    fixed components, writing, signals

--- testbench.vhd
+++ testbench.vhd
@@ -17,26 +17,48 @@ end testbench;
 
 architecture behavior of testbench is
 
-component cache is
-generic(
-    ram_size : INTEGER := 32768
-);
-port(
-    	clock : in std_logic;
-    	reset : in std_logic;
+component instr_mem is
+GENERIC(
+		ram_size : INTEGER := 32768;
+		mem_delay : time := 10 ns;
+		clock_period : time := 1 ns
+	);
+	PORT (
+		clock: IN STD_LOGIC;
+		writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+		address: IN INTEGER RANGE 0 TO 1023;
+		memwrite: IN STD_LOGIC;
+		memread: IN STD_LOGIC;
+		readdata: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+		waitrequest: OUT STD_LOGIC
+	);
+end component;
 
-    --inputs
+component data_memory is 
+GENERIC(
+		ram_size : INTEGER := 32768;
+		clock_period : time := 1 ns
+	);
+	PORT (
+		clock: IN STD_LOGIC;
+		writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+		address: IN INTEGER RANGE 0 TO 8191;
+		memwrite: IN STD_LOGIC;
+		memread: IN STD_LOGIC;
+		readdata: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+		waitrequest: OUT STD_LOGIC
+	);
+end component;
+
+component decode is 
+port(
+	--inputs
 	if_pc : in std_logic_vector(31 downto 0); --program counter
 	if_instr : in std_logic_vector (31 downto 0); --32 bit mips instruction
 	wb_register : in std_logic_vector(31 downto 0); --register to store wb_data
 	wb_data : in std_logic_vector(31 downto 0); --data from writeback stage to put into register
-	
-	mem_register : in std_logic_vector(31 downto 0);
-	mem_data : in std_logic_vector(31 downto 0);
-
-	memwrite: in std_logic_vector(31 downto 0);
-	writedata: in std_logic_vector(31 downto 0);
-
+	clk : in std_logic;
+	reset : in std_logic;
 
 	--outputs for both R and I instructions
 	ex_pc : out std_logic_vector(31 downto 0); --program counter
@@ -46,36 +68,20 @@ port(
 
 	--R instructions
 	ex_regd : out std_logic_vector(31 downto 0); --register d
-	ex_shift : out std_logic_vector(3 downto 0); --shift amount
+	ex_shift : out std_logic_vector(4 downto 0); --shift amount
 	ex_func : out std_logic_vector(5 downto 0); -- function
 
 	--I instructions
-	ex_immed : out std_logic_vector(15 downto 0); --immediate value	
+	ex_immed : out std_logic_vector(31 downto 0); --immediate value	
 
 	--J instructions
 	target : out std_logic_vector(25 downto 0); --branch target
-	-- outputs of operations
-	id_register : out std_logic_vector(31 downto 0);
-	id_data : out std_logic_vector(31 downto 0);
 
-	out_registers: out data_array
-);
-end component;
-
-component memory is 
-GENERIC(
-    ram_size : INTEGER := 32768;
-    mem_delay : time := 10 ns;
-    clock_period : time := 2 ns
-);
-PORT (
-    clock: IN STD_LOGIC;
-    writedata: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-    address: IN INTEGER RANGE 0 TO 8191;
-    memwrite: IN STD_LOGIC;
-    memread: IN STD_LOGIC;
-    readdata: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
-    waitrequest: OUT STD_LOGIC
+	--Data Hazard Detection
+	hazard : out std_logic; --high if hazard
+	
+	--Registers
+	out_registers : out data_array
 );
 end component;
 	
@@ -89,13 +95,6 @@ signal if_pc : std_logic_vector(31 downto 0); --program counter
 signal if_instr : std_logic_vector (31 downto 0); --32 bit mips instruction
 signal wb_register : std_logic_vector(31 downto 0); --register to store wb_data
 signal wb_data : std_logic_vector(31 downto 0); --data from writeback stage to put into register
-
-	
-signal mem_register : std_logic_vector(31 downto 0);
-signal mem_data : std_logic_vector(31 downto 0);
-
-signal memwrite : std_logic_vector(31 downto 0);
-signal writedata : std_logic_vector(31 downto 0);
 
 --outputs for both R and I instructions
 signal ex_pc : std_logic_vector(31 downto 0); --program counter
@@ -114,19 +113,62 @@ signal ex_immed : std_logic_vector(15 downto 0); --immediate value
 --J instructions
 signal target : std_logic_vector(25 downto 0); --branch target
 
-signal id_register : std_logic_vector(31 downto 0);
-signal id_data : std_logic_vector(31 downto 0);
+signal hazard : std_logic;
 
 signal out_registers :  data_array;
+
+signal inst_writedata : STD_LOGIC_VECTOR (31 DOWNTO 0);
+signal inst_address : INTEGER RANGE 0 TO 1023;
+signal inst_memwrite : STD_LOGIC;
+signal inst_memread : STD_LOGIC;
+signal inst_readdata :  STD_LOGIC_VECTOR (31 DOWNTO 0);
+signal inst_waitrequest: STD_LOGIC
+
+signal data_writedata : STD_LOGIC_VECTOR (31 DOWNTO 0);
+signal data_address : INTEGER RANGE 0 TO 8191;
+signal data_memwrite : STD_LOGIC;
+signal data_memread : STD_LOGIC;
+signal data_readdata :  STD_LOGIC_VECTOR (31 DOWNTO 0);
+signal data_waitrequest: STD_LOGIC
+
+signal count : INTEGER RANGE 0 TO 1023;
 
 begin
 
 -- Connect the components which we instantiated above to their
 -- respective signals.
-dut: cache 
+I_MEM: instr_mem 
 port map(
-    	clock => clk,
-    	reset => reset,
+
+clock => clk,
+reset => reset,
+writedata => inst_writedata,
+address => inst_address,
+memwrite => inst_memwrite,
+memread => inst_memread,
+readdata => inst_readdata,
+waitrequest => inst_waitrequest,
+    	
+);
+
+D_MEM: data_memory 
+port map(
+
+clock => clk,
+reset => reset,
+writedata => data_writedata,
+address => data_address,
+memwrite => data_memwrite,
+memread => data_memread,
+readdata => data_readdata,
+waitrequest => data_waitrequest,
+    	
+   
+);
+
+DEC: decode 
+port map(
+
 
     --inputs
 	if_pc => if_pc,
@@ -134,13 +176,6 @@ port map(
 	wb_register => wb_register,
 	wb_data => wb_data,
 	
-	mem_register => mem_register,
-	mem_data => mem_data,
-	
-	memwrite => memwrite,
-	writedata => writedata,
-	
-
 	ex_pc => ex_pc,
 	ex_opcode => ex_opcode,
 	ex_regs => ex_regs, --register s
@@ -156,9 +191,7 @@ port map(
 
 	--J instructions
 	target => target,--branch target
-
-	id_register => id_register,
-	id_data => id_data,
+	hazard => hazard,
 
 	out_registers => out_registers
    
@@ -186,60 +219,64 @@ file 	 register_file:      text;
 variable register_line:      line;
 
 begin
-	--file_open(program,"program.txt", read_mode);
 	
         --init	
         reset <= '1';
         wait for 3*clk_period;
         reset <= '0';
+	count <= '0';
 	
-	--while (not endfile(program)) loop
-	--	if (clk'event and clk = '1') then
-        --        	readline(program, program_line);
-	--		read(program_line, write_data);
-	--		mem_write <= '1';
-	--		wait for clk_period;
-	--		mem_write <= '0';
-	--	end if;
-	--end loop;
-	--file_close(program);
-	--
-	--wait for 9000*clk_period;
---
-	--
-	--file_open(register_file,"register_file.txt",write_mode);
-	--
-	--for i in 0 to 31 loop
-	--	write(register_line, out_registers(i));
-	--	writeline(register_file, register_line);
-	--end loop;
-	--file_close(register_file);
---
+	file_open(program,"program.txt", read_mode);
+	while (not endfile(program)) loop
+		if (clk'event and clk = '1') then
+                	readline(program, program_line);
+			read(program_line, inst_writedata);
+			inst_address <= count;
+			inst_memwrite <= '1';
+			wait for clk_period;
+			inst_memwrite <= '0';
+			count <= count + 1;
+		end if;
+	end loop;
+	file_close(program);
+	
+	wait for 9000*clk_period;
 
-	--file_open(mem_file,"memory.txt",write_mode);
-	---for i in 0 to 8191 loop
-	--	address <= i;
-	--	write(mem_line, readdata);
-	--	writeline(mem_file, mem_line); 
-	--end loop;
-	--file_close(program);
-		
+	
+	file_open(register_file,"register_file.txt",write_mode);
+	
+	for i in 0 to 31 loop
+		write(register_line, out_registers(i));
+		writeline(register_file, register_line);
+	end loop;
+	file_close(register_file);
 
-	wait for clk_period;
-	if_instr <= x"00010005"; --I instruction (addi $1 $0 5)
-	wait for 3*clk_period;
-	assert(ex_regs = x"00000000") report "Register s should contain 0's" severity error;
-	assert(ex_regt = x"00000005") report "Register t should contain value of 5" severity error;
-	if_instr <= x"00020001"; --I instruction (addi $2 $0 1)
-	wait for 3*clk_period;
-	assert(ex_regs = x"00000000") report "Register s should contain 0's" severity error;
-	assert(ex_regt = x"00000001") report "Register t should contain value of 1" severity error;
-	if_instr <= x"00220018";  -- R instruction (mult $1 $2)
-	wait for 10*clk_period;
-	assert(ex_regs = x"00000005") report "Register s (1) should contain value of 5" severity error;
-	assert(ex_regt = x"00000001") report "Register t (2) should contain value of 1" severity error;
-	assert(id_register = x"00000001") report "Result should be stored in register 1" severity error;
-	assert(id_data = x"00000005") report "Result should be 5*1 or 5" severity error;
+	file_open(mem_file,"memory.txt",write_mode);
+	for i in 0 to 8191 loop
+		data_address <= i;
+		data_memread <= '1';
+		wait for 1*clk_period;
+		data_memread <= '0';
+		write(mem_line, data_readdata);
+		writeline(mem_file, mem_line); 
+	end loop;
+	file_close(program);	
+
+	--wait for clk_period;
+	--if_instr <= x"00010005"; --I instruction (addi $1 $0 5)
+	--wait for 3*clk_period;
+	--assert(ex_regs = x"00000000") report "Register s should contain 0's" severity error;
+	--assert(ex_regt = x"00000005") report "Register t should contain value of 5" severity error;
+	--if_instr <= x"00020001"; --I instruction (addi $2 $0 1)
+	--wait for 3*clk_period;
+	--assert(ex_regs = x"00000000") report "Register s should contain 0's" severity error;
+	--assert(ex_regt = x"00000001") report "Register t should contain value of 1" severity error;
+	--if_instr <= x"00220018";  -- R instruction (mult $1 $2)
+	--wait for 10*clk_period;
+	--assert(ex_regs = x"00000005") report "Register s (1) should contain value of 5" severity error;
+	--assert(ex_regt = x"00000001") report "Register t (2) should contain value of 1" severity error;
+	--assert(id_register = x"00000001") report "Result should be stored in register 1" severity error;
+	--assert(id_data = x"00000005") report "Result should be 5*1 or 5" severity error;
 
 	
 	wait;
